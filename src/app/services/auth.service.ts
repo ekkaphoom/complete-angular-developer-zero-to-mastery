0cee4f6e10a4373508e5b3e06a74b7f6fbd5d370
ekkaphoom <ekkaphoom@gmail.com>
2022-07-01 06:02:51 +0700

    119 redirect page is authObly is true

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,9 +4,9 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import IUser from '../models/IUser';
-import { delay, filter, map } from 'rxjs/operators';
+import { delay, filter, map, switchMap } from 'rxjs/operators';
 import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Injectable({
@@ -15,6 +15,7 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthentication$: Observable<boolean>;
+  private isRedirect = false;
 
   constructor(
     private auth: AngularFireAuth,
@@ -27,9 +28,12 @@ export class AuthService {
     this.router.events
       .pipe(
         filter((e) => e instanceof NavigationEnd), //
-        map((e) => this.route.firstChild) //
+        map((e) => this.route.firstChild), //
+        switchMap((route) => route?.data ?? of({}))
       )
-      .subscribe(console.log);
+      .subscribe((data) => {
+        this.isRedirect = data.authOnly ?? false;
+      });
   }
 
   public async createUser(userData: IUser) {
@@ -56,6 +60,8 @@ export class AuthService {
       $event.preventDefault();
     }
     await this.auth.signOut();
-    await this.router.navigateByUrl('/');
+    if (this.isRedirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
